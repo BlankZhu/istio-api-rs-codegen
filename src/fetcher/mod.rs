342d75c26c30de86201228b6276c9823020e014a
BlankZhu <BlankZhu47@gmail.com>
2022-08-29 22:53:24 +0800

    update: add code for fetcher

--- src/fetcher/mod.rs
+++ src/fetcher/mod.rs
@@ -1,17 +1,54 @@
+use log::{info, error};
+use std::time;
 use crate::constant;
 
 #[derive(Debug)]
 pub struct Fetcher {
-
+    client: Option<reqwest::Client>,
 }
 
 impl Fetcher {
     pub fn new() -> Self {
-        return Fetcher {};
+        return Fetcher { client: None };
     }
 
-    pub fn fetch(&self, yaml_body: String) -> Option<String> {
-        todo!()
+    pub fn initialize(&mut self) {
+        let client = reqwest::Client::builder()
+            .timeout(time::Duration::from_secs(10))
+            .build();
+        match client {
+            Ok(c) => self.client = Some(c),
+            Err(e) => panic!("failed to intialize reqwest client: {}", e),
+        }
+    }
+
+    pub async fn fetch(&self, istio_version: &str) -> Option<String> {
+        let url = [
+            constant::ISTIO_CRD_ALL_URL_PREFIX,
+            istio_version,
+            constant::ISTIO_CRD_ALL_URL_SUFFIX,
+        ]
+        .join("");
+
+        info!("fetching all-in-one yaml from {}", url.as_str());
+
+        let resp = self.client.as_ref()?.get(url).send().await;
+        match resp {
+            Ok(resp) => {
+                let text_body = resp.text().await;
+                match text_body {
+                    Ok(text) => return Some(text),
+                    Err(e) => {
+                        error!("failed to read body: {}", e);
+                        return None;
+                    }
+                }
+            },
+            Err(e) => {
+                error!("failed to fetch yaml: {}", e);
+                return None;
+            }
+        }
     }
 
     // fn start_fetching(&self) {
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
+use log::info;
+
 mod constant;
 mod fetcher;
 mod resolver;
 mod generator;
 mod option;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    env_logger::init();
+    
+    info!("istio-api-rs-codegen now running!");
+
+    let mut fetcher = fetcher::Fetcher::new();
+    fetcher.initialize();
+
+    let mut fetch_promises = Vec::new();
+    constant::ISTIO_VERSIONS.iter().for_each(|version| {
+        fetch_promises.push(fetcher.fetch(version));
+    });
+    for promise in fetch_promises {
+        let resp = promise.await;
+        if let Some(yaml) = resp {
+            println!("{}", yaml);
+        }
+    }
+
+
+    Ok(())
 }
