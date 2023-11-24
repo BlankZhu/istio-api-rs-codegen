f506616021cd37cf9abe7ff4b4e81477565e0021
BlankZhu <BlankZhu47@gmail.com>
2023-11-24 09:28:07 +0800

    update: add 1.20 support

--- src/constant/mod.rs
+++ src/constant/mod.rs
@@ -1,4 +1,7 @@
-pub static ISTIO_VERSIONS: &'static [&str] = &["1.10.0", "1.11.0", "1.12.0", "1.13.0", "1.14.0", "1.15.0", "1.16.0", "1.17.0", "1.18.0", "1.19.0"];
+pub static ISTIO_VERSIONS: &'static [&str] = &[
+    "1.10.0", "1.11.0", "1.12.0", "1.13.0", "1.14.0", "1.15.0", "1.16.0", "1.17.0", "1.18.0",
+    "1.19.0", "1.20.0",
+];
 pub static ISTIO_CRD_ALL_URL_PREFIX: &'static str =
     "https://raw.githubusercontent.com/istio/istio/";
 pub static ISTIO_CRD_ALL_URL_SUFFIX: &'static str = "/manifests/charts/base/crds/crd-all.gen.yaml";
